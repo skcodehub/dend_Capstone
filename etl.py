d93e55504d730ddc1a8a475d0cdea77e657d9e94
skcodehub <44783634+skcodehub@users.noreply.github.com>
2020-03-28 16:59:50 -0700

    Changes
    sparksession changes

--- etl.py
+++ etl.py
@@ -3,6 +3,13 @@ import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 import datetime
 from pyspark.sql import SparkSession
+import os
+
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['keypair']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['keypair']['AWS_SECRET_ACCESS_KEY']
 
 def create_spark_session():
     """
@@ -44,7 +51,7 @@ def load_date_dim(cur, conn):
         date_id= date_id-1
         start_date = start_date-interval
 
-def load_dim_tables(spark, cur, conn, config):
+def load_dim_tables(spark, cur, conn):
     """
     This function builds dimension tables
     Parameters: This function requires spark session, connection, connection cursor and config as parameters
@@ -54,12 +61,15 @@ def load_dim_tables(spark, cur, conn, config):
                     ) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """)
 
     #path=config['S3']['DEMOGR_DATA'] +'/us-cities-demographics.csv'
-    df = spark.read.csv('s3://capstoneimmi/demo/us-cities-demographics.csv').options(header='true', delimiter=';')
+    #df = spark.read.csv('s3://capstoneimmi/demo/us-cities-demographics.csv').options(header='true', delimiter=';')
    # df_demogr = df[['City', 'State Code', 'State', 'Median Age', 'Male Population', 'Female Population', 'Total Population', 'Number of Veterans', 'Foreign-born', 'Average Household Size', 'Race', 'Count']].dropDuplicates()
-    df.head()
+    #df.head()
    # for i, row in df_demogr.iterrows():
     #    cur.execute(demo_dim_insert, row)
-        
+    
+    df = spark.read.csv("s3://capstoneimmi/mode/I94Mode.csv").options(header='true')
+    df.head()
+    
 def main():
     """
     Connect to the database and call functions to copy data into
@@ -67,8 +77,6 @@ def main():
     Reads the configuration params from the config file.
     Parameters: This function requires connection and connection cursor as parameters.
     """
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
     spark = create_spark_session()
     
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
@@ -76,7 +84,7 @@ def main():
     
     #load_staging_tables(cur, conn)
     #load_date_dim(cur, conn)
-    load_dim_tables(spark,cur,conn, config)
+    load_dim_tables(spark,cur,conn)
 
     conn.close()
 
